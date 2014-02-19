fb5bbcc3664214f0506c78796ee105ba6a036ab7
Philippe O. Wagner <wagner@arteria.ch>
2014-02-19 11:11:52 +0100

    fixed import in urls.

--- mobileapps_store/urls.py
+++ mobileapps_store/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('mobilealls_store.views',
     url(r'^$', view='switchToStoreOrLanding', name='switch_to_store_or_landing'),
