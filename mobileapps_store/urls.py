a062cf274d3ed03eb9d01a7529fdcc8208126a6a
Philippe O. Wagner <wagner@arteria.ch>
2014-02-19 11:12:55 +0100

    fixed type - again - grr.

--- mobileapps_store/urls.py
+++ mobileapps_store/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
 
-urlpatterns = patterns('mobilealls_store.views',
+urlpatterns = patterns('mobileapps_store.views',
     url(r'^$', view='switchToStoreOrLanding', name='switch_to_store_or_landing'),
 )
